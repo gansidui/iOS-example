d0e240173e594c199d28e35ed7ce51d12aefaf85
Jie Li <lijiegm@gmail.com>
2018-05-19 16:35:31 +0800

    init

---
+++ HelloAV/HelloAV/HelloAV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloAV
+//
+//  Created by lijie on 2018/5/19.
+//  Copyright © 2018年 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloAV/HelloAV/HelloAV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloAV
+//
+//  Created by lijie on 2018/5/19.
+//  Copyright © 2018年 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
